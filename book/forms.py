c68f926b5345c3f9566ec19e0a4e33658e9f4cc6
samiulalimsaad <samiulalimsaad@gmail.com>
2024-02-08 12:51:56 +0600

    user form added

--- book/forms.py
+++ book/forms.py
@@ -1,4 +1,6 @@
 from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 
 from .models import Review
 
@@ -7,3 +9,23 @@ class ReviewForm(forms.ModelForm):
     class Meta:
         model = Review
         fields = ["text"]
+
+
+class UserRegistrationForm(UserCreationForm):
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = ["username", "email", "password1", "password2"]
+
+
+class DepositForm(forms.Form):
+    amount = forms.DecimalField(label="Amount to Deposit")
+
+
+class BorrowForm(forms.Form):
+    book_id = forms.IntegerField(label="Book ID")
+
+
+class ReturnForm(forms.Form):
+    book_id = forms.IntegerField(label="Book ID")
