d46ae279483d31f6bf637a2decbc4811dd36331b
samiulalimsaad <samiulalimsaad@gmail.com>
2024-02-08 12:12:38 +0600

    models added

--- book/models.py
+++ book/models.py
@@ -1,3 +1,4 @@
+# models.py
 from django.contrib.auth.models import User
 from django.db import models
 
@@ -5,17 +6,25 @@ from django.db import models
 class Category(models.Model):
     name = models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.name
+
 
 class Book(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
     image = models.ImageField(upload_to="book_images/")
     borrowing_price = models.DecimalField(max_digits=10, decimal_places=2)
-    reviews = models.ManyToManyField(User, through="Review")
     categories = models.ManyToManyField(Category)
+
+    def __str__(self):
+        return self.title
 
 
 class Review(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     book = models.ForeignKey(Book, on_delete=models.CASCADE)
     text = models.TextField()
+
+    def __str__(self):
+        return f"{self.user.username}'s review on {self.book.title}"
