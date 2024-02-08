d7c80223975e1a261fe53fad98dab7addeecd093
samiulalimsaad <samiulalimsaad@gmail.com>
2024-02-08 12:12:50 +0600

    admin added

--- book/admin.py
+++ book/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from book.models import Book, Category, Review
+
 # Register your models here.
+
+admin.site.register(Book)
+admin.site.register(Category)
+admin.site.register(Review)
