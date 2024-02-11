0536efaca2356cea1abc94f05829963978e751a7
samiulalimsaad <samiulalimsaad@gmail.com>
2024-02-11 12:06:36 +0600

    profile view added

--- book/views.py
+++ book/views.py
@@ -1,11 +1,19 @@
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.models import User
 from django.shortcuts import redirect, render
 from django.urls import reverse_lazy
 from django.views import View
-from django.views.generic import DetailView, FormView, ListView
+from django.views.generic import DetailView, FormView, ListView, UpdateView
 
-from .forms import BorrowForm, DepositForm, ReturnForm, ReviewForm, UserRegistrationForm
+from .forms import (
+    BorrowForm,
+    DepositForm,
+    ReturnForm,
+    ReviewForm,
+    UserProfileForm,
+    UserRegistrationForm,
+)
 from .models import Book
 
 
@@ -24,14 +32,11 @@ class BorrowBookView(LoginRequiredMixin, View):
         return render(request, self.template_name, {"book": book})
 
     def post(self, request, *args, **kwargs):
-        # Handle borrowing logic
-        # Update user balance, create borrowing record, etc.
         return redirect("book_list")
 
 
 class ReturnBookView(LoginRequiredMixin, View):
     def post(self, request, *args, **kwargs):
-        # Handle returning logic
         return redirect("user_profile")
 
 
@@ -51,18 +56,12 @@ class BookDetailView(LoginRequiredMixin, DetailView):
 
 
 class UserRegistrationView(FormView):
-    """
-    Class-based view for user registration.
-    """
 
     template_name = "users/registration.html"
     form_class = UserRegistrationForm
     success_url = reverse_lazy("book_list")
 
     def form_valid(self, form):
-        """
-        If the form is valid, save the user instance and authenticate the user.
-        """
         user = form.save()
         username = form.cleaned_data.get("username")
         password = form.cleaned_data.get("password1")
@@ -90,3 +89,24 @@ class ReturnView(FormView):
     template_name = "return.html"
     form_class = ReturnForm
     success_url = reverse_lazy("return_success")
+
+
+class UserProfileDetailView(LoginRequiredMixin, DetailView):
+
+    model = User
+    template_name = "users/profile.html"
+    context_object_name = "user"
+
+    def get_object(self, queryset=None):
+        return self.request.user
+
+
+class UserProfileUpdateView(UpdateView):
+
+    model = User
+    form_class = UserProfileForm
+    template_name = "users/profile_update.html"
+    success_url = reverse_lazy("profile_update_success")
+
+    def get_object(self, queryset=None):
+        return self.request.user
