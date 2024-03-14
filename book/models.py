0f6e6df8d35c841adc92ddc17745bbac23355eec
samiulalimsaad <samiulalimsaad@gmail.com>
2024-03-14 17:15:45 +0600

    UserAccount and BorrowingHistory  models added

--- book/models.py
+++ book/models.py
@@ -25,6 +25,19 @@ class Review(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     book = models.ForeignKey(Book, on_delete=models.CASCADE)
     text = models.TextField()
+    rating = models.IntegerField()
 
     def __str__(self):
         return f"{self.user.username}'s review on {self.book.title}"
+
+
+class UserAccount(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    deposit_amount = models.DecimalField(max_digits=8, decimal_places=2)
+
+
+class BorrowingHistory(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    borrowing_date = models.DateField(auto_now_add=True)
+    return_date = models.DateField(null=True, blank=True)
